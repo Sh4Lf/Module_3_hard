de80fa920f920cfbfcf1f2cce29213ea0850d5d0
Sh4Lf <sergeinemchinov@mail.ru>
2024-10-09 17:26:09 +0600

    Add files via upload

---
+++ Module_3_hard.py
@@ -0,0 +1,27 @@
+def calculate_structure_sum(*args):
+    total_sum = 0
+
+    for element in args:
+        if isinstance(element, (int, float)):
+            total_sum += element
+        elif isinstance(element, str):
+            total_sum += len(element)
+        elif isinstance(element, dict):
+            total_sum += calculate_structure_sum(*element.keys())
+            total_sum += calculate_structure_sum(*element.values())
+        elif isinstance(element, (list, set, tuple)):
+            total_sum += calculate_structure_sum(*element)
+
+    return total_sum
+
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
